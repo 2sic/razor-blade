43016e8a92e4c6a72697550b99c1df31855b83b6
ijungleboy <daniel.mettler@2sic.com>
2019-10-19 12:02:30 +0200

    rebuild docs

--- Razor Blades Tests/TagTests/TagAttributeTests.cs
+++ Razor Blades Tests/TagTests/TagAttributeTests.cs
@@ -11,19 +11,19 @@ namespace Razor_Blades_Tests.TagTests
         [TestMethod]
         public void TagAttributeBasics()
         {
-            Is("<div class='x'>", Tag.Other("div").Attr("class", "x").TagStart);
+            Is("<div class='x'>", Tag.Custom("div").Attr("class", "x").TagStart);
         }
 
         [TestMethod]
         public void TagAttributeStandalone()
         {
-            Is("<div data-fancybox>", Tag.Other("div").Attr("data-fancybox").TagStart);
+            Is("<div data-fancybox>", Tag.Custom("div").Attr("data-fancybox").TagStart);
         }
 
         [TestMethod]
         public void TagAttributeMultiple()
         {
-            Is("<div class='x' name='value'>", Tag.Other("div")
+            Is("<div class='x' name='value'>", Tag.Custom("div")
                 .Attr("class", "x")
                 .Attr("name", "value")
                 .TagStart
@@ -33,7 +33,7 @@ namespace Razor_Blades_Tests.TagTests
          [TestMethod]
         public void TagAttributeAppend()
         {
-            Is("<div class='xyz'>", Tag.Other("div")
+            Is("<div class='xyz'>", Tag.Custom("div")
                 .Attr("class", "x", "")
                 .Attr("class", "y", "")
                 .Attr("class", "z", "")
@@ -44,7 +44,7 @@ namespace Razor_Blades_Tests.TagTests
         [TestMethod]
         public void TagAttributeSeparator()
         {
-            Is("<div class='x,y-z'>", Tag.Other("div")
+            Is("<div class='x,y-z'>", Tag.Custom("div")
                 .Attr("class", "x", " ")
                 .Attr("class", "y", ",")
                 .Attr("class", "z", "-")
@@ -55,7 +55,7 @@ namespace Razor_Blades_Tests.TagTests
         [TestMethod]
         public void TagAttributeReplace()
         {
-            Is("<div class='z'>", Tag.Other("div")
+            Is("<div class='z'>", Tag.Custom("div")
                 .Attr("class", "x", " ")
                 .Attr("class", "y", ",")
                 .Attr("class", "z", null)
@@ -68,30 +68,30 @@ namespace Razor_Blades_Tests.TagTests
         [TestMethod]
         public void TagId()
         {
-            Is("<div id='z'>", Tag.Other("div").Id("z").TagStart);
-            Is("<div id='y'>", Tag.Other("div").Id("z").Id("y").TagStart);
-            Is("<div id>", Tag.Other("div").Id(null).TagStart);
-            Is("<div id>", Tag.Other("div").Id("z").Id(null).TagStart);
-            Is("<div id='x'>", Tag.Other("div").Id("z").Id(null).Id("x").TagStart);
+            Is("<div id='z'>", Tag.Custom("div").Id("z").TagStart);
+            Is("<div id='y'>", Tag.Custom("div").Id("z").Id("y").TagStart);
+            Is("<div id>", Tag.Custom("div").Id(null).TagStart);
+            Is("<div id>", Tag.Custom("div").Id("z").Id(null).TagStart);
+            Is("<div id='x'>", Tag.Custom("div").Id("z").Id(null).Id("x").TagStart);
         }
 
         [TestMethod]
         public void TagClass()
         {
-            Is("<div class='z'>", Tag.Other("div").Class("z").TagStart);
-            Is("<div class='z y'>", Tag.Other("div").Class("z").Class("y").TagStart);
-            Is("<div class>", Tag.Other("div").Class(null).TagStart);
-            Is("<div class>", Tag.Other("div").Class("z").Class("y").Class(null).TagStart);
-            Is("<div class='x'>", Tag.Other("div").Class("z").Class("y").Class(null).Class("x").TagStart);
+            Is("<div class='z'>", Tag.Custom("div").Class("z").TagStart);
+            Is("<div class='z y'>", Tag.Custom("div").Class("z").Class("y").TagStart);
+            Is("<div class>", Tag.Custom("div").Class(null).TagStart);
+            Is("<div class>", Tag.Custom("div").Class("z").Class("y").Class(null).TagStart);
+            Is("<div class='x'>", Tag.Custom("div").Class("z").Class("y").Class(null).Class("x").TagStart);
         }
 
         [TestMethod]
         public void TagStyle()
         {
-            Is("<div style='z'>", Tag.Other("div").Style("z").TagStart);
-            Is("<div style='z;y'>", Tag.Other("div").Style("z").Style("y").TagStart);
-            Is("<div style>", Tag.Other("div").Style("z").Style("y").Style(null).TagStart);
-            Is("<div style='x'>", Tag.Other("div").Style("z").Style("y").Style(null).Style("x").TagStart);
+            Is("<div style='z'>", Tag.Custom("div").Style("z").TagStart);
+            Is("<div style='z;y'>", Tag.Custom("div").Style("z").Style("y").TagStart);
+            Is("<div style>", Tag.Custom("div").Style("z").Style("y").Style(null).TagStart);
+            Is("<div style='x'>", Tag.Custom("div").Style("z").Style("y").Style(null).Style("x").TagStart);
         }
         #endregion
     }
--- Razor.Blade/Blade/Tag/Tag_Custom.cs
+++ Razor.Blade/Blade/Tag/Tag_Custom.cs
@@ -6,12 +6,12 @@
         /// <summary>
         /// Create a generic html tag for a specific name.
         /// We recommend to use the normal tag overloads instead.
-        /// So Tag.Img(...) is better than Tag.Other("Img") because it's preconfigured to self-close, encode the src etc.
+        /// So Tag.Img(...) is better than Tag.Custom("Img") because it's preconfigured to self-close, encode the src etc.
         /// </summary>
         /// <param name="name">tag name</param>
         /// <param name="content">optional content to place within the tag - can be a string or other tags</param>
         /// <returns>HtmlString of the tag, so you can use it directly with @TagBase.TagStart(...) in your razor</returns>
-        public static Markup.TagBase Other(string name, params object[] content)
+        public static Markup.TagBase Custom(string name, params object[] content)
             => new Markup.TagBase(name, content);
 
     }
