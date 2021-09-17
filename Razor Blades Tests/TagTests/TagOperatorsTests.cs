32cd85ab43b9da50d4020a423b0c4c29fbd73141
ijungleboy <daniel.mettler@2sic.com>
2021-09-18 00:28:29 +0200

    Implement + operator for TagBase, Strings and Boolean fix #10

---
+++ Razor Blades Tests/TagTests/TagOperatorsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToSic.Razor.Blade;
+using ToSic.Razor.Markup;
+
+namespace Razor_Blades_Tests.TagTests
+{
+    [TestClass]
+    public class TagOperatorsTests
+    {
+        [TestMethod]
+        public void AddTags()
+        {
+            var addTags = Tag.Div() + Tag.P();
+            Assert.IsInstanceOfType(addTags, typeof(TagBase));
+            Assert.AreEqual("<div></div><p></p>", addTags.ToString());
+
+            var addTags2 = Tag.Div("a") + Tag.P("b");
+            Assert.IsInstanceOfType(addTags2, typeof(TagBase));
+            Assert.AreEqual("<div>a</div><p>b</p>", addTags2.ToString());
+        }
+
+        [TestMethod]
+        public void AddTagsWithStrings()
+        {
+            var addTags = "<div></div>" + Tag.P();
+            Assert.IsInstanceOfType(addTags, typeof(string));
+            Assert.AreEqual("<div></div><p></p>", addTags);
+
+            var addTags2 = Tag.Div("a") + "<p>b</p>";
+            Assert.IsInstanceOfType(addTags2, typeof(string));
+            Assert.AreEqual("<div>a</div><p>b</p>", addTags2);
+        }
+
+        [TestMethod]
+        public void AddTagsWithNumbers()
+        {
+            var addTags = 5 + Tag.P();
+            Assert.IsInstanceOfType(addTags, typeof(string));
+            Assert.AreEqual("5<p></p>", addTags);
+
+            var addTags2 = Tag.Div("a") + 27;
+            Assert.IsInstanceOfType(addTags2, typeof(string));
+            Assert.AreEqual("<div>a</div>27", addTags2);
+        }
+
+        [TestMethod]
+        public void AddTagsWithBoolean()
+        {
+            var addTags = true + Tag.P();
+            Assert.IsInstanceOfType(addTags, typeof(string));
+            Assert.AreEqual("true<p></p>", addTags);
+
+            var addTags2 = Tag.Div("a") + false;
+            Assert.IsInstanceOfType(addTags2, typeof(string));
+            Assert.AreEqual("<div>a</div>false", addTags2);
+        }
+    }
+}
---
+++ Razor.Blade/Markup/Tag/TagBase_Operators.cs
@@ -0,0 +1,22 @@
+﻿namespace ToSic.Razor.Markup
+{
+    public partial class TagBase
+    {
+
+        public static TagBase operator +(TagBase a) => a;
+
+        public static TagBase operator +(TagBase a, TagBase b) => new TagCustom().Wrap(a, b);
+
+        public static string operator +(string a, TagBase b) => a + b.ToString();
+
+        public static string operator +(TagBase a, string b) => a.ToString() + b;
+
+        public static string operator +(int a, TagBase b) => a + b.ToString();
+
+        public static string operator +(TagBase a, int b) => a.ToString() + b;
+
+        public static string operator +(bool a, TagBase b) => a.ToString().ToLower() + b.ToString();
+
+        public static string operator +(TagBase a, bool b) => a.ToString() + b.ToString().ToLower();
+    }
+}
