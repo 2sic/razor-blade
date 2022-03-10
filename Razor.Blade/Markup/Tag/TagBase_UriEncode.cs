e70835c1fd6f8cb028130dab44cf0fa1e4dfd776
ijungleboy <daniel.mettler@2sic.com>
2022-03-10 20:50:35 +0100

    releasing

--- Razor.Blade/Markup/Tag/TagBase_UriEncode.cs
+++ Razor.Blade/Markup/Tag/TagBase_UriEncode.cs
@@ -1,11 +1,14 @@
 ﻿using ToSic.Razor.Internals;
+using ToSic.Razor.Internals.Documentation;
 
 namespace ToSic.Razor.Markup
 {
     public partial class TagBase
     {
+        [PrivateApi("Should remove soon, after auto-generated code is updated")]
         internal static string UriEncode(string url) => UriHelpers.UriEncode(url);
 
+        [PrivateApi("Should remove soon, after auto-generated code is updated")]
         internal static string UriEncodeSrcSet(string srcset) => UriHelpers.UriEncodeSrcSet(srcset);
 
     }
