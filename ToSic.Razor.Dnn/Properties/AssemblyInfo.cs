74765a154539399a9afe6dccc8acbafd17797b19
ijungleboy <daniel.mettler@2sic.com>
2022-03-10 20:47:42 +0100

    Version bump 03.11

--- ToSic.Razor.Dnn/Properties/AssemblyInfo.cs
+++ ToSic.Razor.Dnn/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("03.10.0.00")]
-[assembly: AssemblyFileVersion("03.10.0.00")]
+[assembly: AssemblyVersion("03.11.0.00")]
+[assembly: AssemblyFileVersion("03.11.0.00")]
