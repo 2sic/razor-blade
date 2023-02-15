c2efd88447efeda50ee0f299dc90d95e344194d5
Tonći Vatavuk <tonci@sistemi.hr>
2023-02-15 18:27:49 +0100

    unit test for https://github.com/2sic/2sxc/issues/2998

--- ToSic.RazorBladeTests/HtmlTests/HtmlToJsonTests.cs
+++ ToSic.RazorBladeTests/HtmlTests/HtmlToJsonTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToSic.Razor.Internals;
 
 namespace ToSic.RazorBladeTests.HtmlTests
@@ -6,15 +7,25 @@ namespace ToSic.RazorBladeTests.HtmlTests
     [TestClass]
     public class HtmlToJsonTests
     {
+        public Func<object, string> ToJson = Html.ToJson;
+
         [TestMethod]
         public void Basic()
         {
-            Assert.AreEqual("null", Html.ToJson(null));
-            Assert.AreEqual("45", Html.ToJson(45));
-            Assert.AreEqual("{\"Name\":\"Daniel\"}", Html.ToJson(new { Name = "Daniel"}));
+            Assert.AreEqual("null", ToJson(null));
+            Assert.AreEqual("45", ToJson(45));
+            Assert.AreEqual("{\"Name\":\"Daniel\"}", ToJson(new { Name = "Daniel"}));
+        }
+
+        [TestMethod]
+        public void DateTimeSerialization()
+        {
+            Assert.AreEqual("\"2023-02-15T18:26:15Z\"", ToJson(new DateTime(2023,2,15, 18, 26, 15)));
+            Assert.AreEqual("{\"datePosted\":\"2023-02-15T00:00:00Z\"}", ToJson(new { datePosted = new DateTime(2023, 2, 15) }));
         }
 
         public HtmlToJsonTests BadPropertyToPreventSerialization;
+        
 
         // 2022-02-01 As tests were moved to .net 6, the standard .net json serializer ignores bad properties so these tests don't throw errors any more
         //[TestMethod]
